decff439969ca518259d3e79240f5ea61020add9
ItIsIEli <eli.h.pope11@gmail.com>
2024-06-24 16:21:03 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const FootBall = SpriteKind.create()
+    export const SkateBoard = SpriteKind.create()
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     game.gameOver(false)
 })
@@ -9,12 +13,42 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.vy = -150
     }
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
     game.gameOver(true)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     game.gameOver(false)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.FootBall, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.SkateBoard, function (sprite, otherSprite) {
+    Unsharpenedpencil = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        f f f f f f f f f f f f f f f f 
+        3 3 b 5 5 5 5 5 5 5 5 5 d d d f 
+        3 3 b 5 5 5 5 5 5 5 5 5 5 d d d 
+        3 3 b 5 5 5 5 5 5 5 5 5 5 d d d 
+        f f f f f f f f f f f f f f f f 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+})
+let Unsharpenedpencil: Sprite = null
+let Skate_Board: Sprite = null
+let Foot_Ball: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(9)
 mySprite = sprites.create(img`
@@ -39,3 +73,172 @@ controller.moveSprite(mySprite, 100, 0)
 tiles.setCurrentTilemap(tilemap`level2`)
 scene.cameraFollowSprite(mySprite)
 mySprite.ay = 300
+for (let value of tiles.getTilesByType(assets.tile`myTile5`)) {
+    Foot_Ball = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f d d d f f . . . . 
+        . . f f d d d e e e e e f f . . 
+        . f e e e e e e e e e e e e f . 
+        f e e e e e e e e e e e e e e f 
+        . f e e e e e e e e e e e e f . 
+        . . f f e e e e e e e e f f . . 
+        . . . . f f f e e e f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.FootBall)
+    animation.runImageAnimation(
+    Foot_Ball,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f d d d f f . . . . 
+        . . f f d d d e e e e e f f . . 
+        . f e e e e e e e e e e e e f . 
+        f e e e e e e e e e e e e e e f 
+        . f e e e e e e e e e e e e f . 
+        . . f f e e e e e e e e f f . . 
+        . . . . f f f e e e f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f e e e f f . . . . 
+        . . f f e e e d d d d d f f . . 
+        . f d d d d d e e e e e e e f . 
+        f e e e e e e e e e e e e e e f 
+        . f e e e e e e e e e e e e f . 
+        . . f f e e e e e e e e f f . . 
+        . . . . f f f e e e f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f e e e f f . . . . 
+        . . f f e e e e e e e e f f . . 
+        . f e e e e e e e e e e e e f . 
+        f d d d d d d d d d d d d d d f 
+        . f e e e e e e e e e e e e f . 
+        . . f f e e e e e e e e f f . . 
+        . . . . f f f e e e f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f e e e f f . . . . 
+        . . f f e e e e e e e e f f . . 
+        . f e e e e e e e e e e e e f . 
+        f e e e e e e e e e e e e e e f 
+        . f d d d d d e e e e e e e f . 
+        . . f f e e e d d d d d f f . . 
+        . . . . f f f e e e f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f e e e f f . . . . 
+        . . f f e e e e e e e e f f . . 
+        . f e e e e e e e e e e e e f . 
+        f e e e e e e e e e e e e e e f 
+        . f e e e e e e e e e e e e f . 
+        . . f f d d d e e e e e f f . . 
+        . . . . f f f d d d f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f e e e f f . . . . 
+        . . f f e e e e e e e e f f . . 
+        . f e e e e e e e e e e e e f . 
+        f e e e e e e e e e e e e e e f 
+        . f e e e e e e e e e e e e f . 
+        . . f f e e e e e e e e f f . . 
+        . . . . f f f e e e f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . f f f e e e f f . . . . 
+        . . f f e e e e e e e e f f . . 
+        . f e e e e e e e e e e e e f . 
+        f e e e e e e e e e e e e e e f 
+        . f e e e e e e e e e e e e f . 
+        . . f f e e e e e e e e f f . . 
+        . . . . f f f e e e f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    200,
+    true
+    )
+    tiles.placeOnTile(Foot_Ball, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+for (let value of tiles.getTilesByType(assets.tile`myTile6`)) {
+    Skate_Board = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . f . . . . . . . . . . . . f . 
+        . f f . . . . . . . . . . f f . 
+        . e f f f f f f f f f f f f e . 
+        . . e b b e e e e e e b b e . . 
+        . . . b b . . . . . . b b . . . 
+        `, SpriteKind.SkateBoard)
+    tiles.placeOnTile(Skate_Board, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
