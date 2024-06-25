28af20014a0e539a2d1db062882458653842a2a7
ItIsIEli <eli.h.pope11@gmail.com>
2024-06-25 06:07:06 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 namespace SpriteKind {
     export const FootBall = SpriteKind.create()
     export const SkateBoard = SpriteKind.create()
+    export const ball = SpriteKind.create()
+    export const bAt = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     game.gameOver(false)
@@ -13,15 +15,15 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.vy = -150
     }
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
-    game.gameOver(true)
-})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     game.gameOver(false)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.FootBall, function (sprite, otherSprite) {
     info.changeScoreBy(2)
     sprites.destroy(otherSprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.bAt, function (sprite, otherSprite) {
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(81, 10))
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.SkateBoard, function (sprite, otherSprite) {
     sprites.destroy(otherSprite)
@@ -69,6 +71,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.SkateBoard, function (sprite, ot
     Unsharpenedpencil.setPosition(mySprite.x + 60, mySprite.y + -60)
     Unsharpenedpencil.follow(mySprite)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.ball, function (sprite, otherSprite) {
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(41, 12))
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(otherSprite)
     if (mySprite.y < otherSprite.y) {
@@ -78,6 +83,8 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     }
 })
 let Unsharpenedpencil: Sprite = null
+let BAT: Sprite = null
+let BALL: Sprite = null
 let Skate_Board: Sprite = null
 let Foot_Ball: Sprite = null
 let mySprite: Sprite = null
@@ -272,5 +279,32 @@ for (let value of tiles.getTilesByType(assets.tile`myTile6`)) {
         . . . b b . . . . . . b b . . . 
         `, SpriteKind.SkateBoard)
     tiles.placeOnTile(Skate_Board, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+for (let value of tiles.getTilesByType(assets.tile`myTile7`)) {
+    BALL = sprites.create(assets.tile`myTile4`, SpriteKind.ball)
+    tiles.placeOnTile(BALL, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+for (let value of tiles.getTilesByType(assets.tile`myTile8`)) {
+    BAT = sprites.create(img`
+        . . . . . . . . . . . f f f . . 
+        . . . . . . . . . . f f d f f . 
+        . . . . . . . . . f f d d d f f 
+        . . . . . . . . . f d d d d d f 
+        . . . . . . . . f d d d d d f f 
+        . . . . . . . f d d d d d f f . 
+        . . . . . . f d d d d d f f . . 
+        . . . . . . f d d d d f . . . . 
+        . . . . . f b d d d f . . . . . 
+        . . . . f b b d f f . . . . . . 
+        . . . . f b b f . . . . . . . . 
+        . . . f b f f . . . . . . . . . 
+        . f f f f . . . . . . . . . . . 
+        f e f f . . . . . . . . . . . . 
+        f e e f . . . . . . . . . . . . 
+        . f f . . . . . . . . . . . . . 
+        `, SpriteKind.bAt)
+    tiles.placeOnTile(BAT, value)
     tiles.setTileAt(value, assets.tile`transparency16`)
 }
